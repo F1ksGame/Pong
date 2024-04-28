69ab5d33bae52b44f50abc492af3de01a05736d8
F1ksGame <167691399+F1ksGame@users.noreply.github.com>
2024-04-28 16:57:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,63 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+#мяч и ракетка
+recket1 = Player('racket.png', 30, 200, 4, 50, 150)
+recket2 = Player('racet.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+#если мяч улетео дальше ракетки, выводим условие проигрыша для первого игрока
+if ball.rect.x < 0:
+finish = Truewindow.bilst(lose2, (200, 200))
+game_over = True
+
+#если мяч улетео дальше ракетки, выводим условие проигрыша для второго игрока
+if ball.rect.x < 0:
+finish = Truewindow.bilst(lose2, (200, 200))
+game_over = True
+
+
+
+
+while game:
+    for i in event.get():
+        if i.tyoe == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+        speed_y *= 1
+    
 #экран
 back = (200, 255, 255)
 win_width = 600
@@ -26,37 +83,17 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
-#флаги, отвечающие за состояние игры
-game = True
-finish = False
-clock = time.Clock()
-FPS = 60
-
-#мяч и ракетка
-recket1 = Player('racket.png', 30, 200, 4, 50, 150)
-recket2 = Player('racet.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
-
-
-
-#если мяч улетео дальше ракетки, выводим условие проигрыша для первого игрока
-if ball.rect.x < 0:
-    finish = Truewindow.bilst(lose2, (200, 200))
-    game_over = True
-
-#если мяч улетео дальше ракетки, выводим условие проигрыша для второго игрока
-if ball.rect.x < 0:
-    finish = Truewindow.bilst(lose2, (200, 200))
-    game_over = True
-
-recket1.reset()
-recket2.reset()
-ball.reset()
 
 
 run = True
+finish = False
 while run: 
     for i in event.get():
         if e.tyoe == QUIT:
             run = False
-    window.blit(background 0, 0)
+    if not finish:
+        ball.reset()
+        display.update()
+    clock.tick(FPS)
+
+display.update()
